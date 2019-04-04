5c345f7792a9ef46cc44543e6423b587884420a4
Berghold-Wieser Thomas <thomas.berghold-wieser@tieto.com>
2019-04-04 08:41:50 +0200

    ChargeAfterAuthorization Tests added

--- test/Heidelpay.Payment.External.Tests/Business/ChargeAfterAuthorizationTests.cs
+++ test/Heidelpay.Payment.External.Tests/Business/ChargeAfterAuthorizationTests.cs
@@ -5,10 +5,56 @@ namespace Heidelpay.Payment.External.Tests.Business
 {
     public class ChargeAfterAuthorizationTests : PaymentTypeTestsBase
     {
-        [Fact(Skip = "It's not ready yet")]
-        public async Task EmptyTest()
+        [Fact]
+        public async Task Fetch_Authorization()
         {
-            await Task.CompletedTask;
+            var card = await Heidelpay.CreatePaymentTypeAsync(PaymentTypeCardNo3DS);
+            var auth = await Heidelpay.AuthorizeAsync(new Authorization(card) { Amount = 1m, Currency = "EUR", ReturnUrl = TestReturnUri });
+            var fetched = await Heidelpay.FetchAuthorizationAsync(auth.PaymentId);
+
+            Assert.NotNull(fetched?.Id);
+        }
+
+        [Fact]
+        public async Task Full_Charge_After_Authorization()
+        {
+            var orderId = GetRandomId();
+
+            var card = await Heidelpay.CreatePaymentTypeAsync(PaymentTypeCardNo3DS);
+            var auth = await Heidelpay.AuthorizeAsync(new Authorization(card) { Amount = 1m, Currency = "EUR", ReturnUrl = TestReturnUri, OrderId = orderId });
+            var fetched = await Heidelpay.FetchAuthorizationAsync(auth.PaymentId);
+            var charge = await fetched.ChargeAsync();
+
+            Assert.NotNull(charge?.Id);
+            Assert.Equal(orderId, auth.OrderId);
+            Assert.Equal(orderId, fetched.OrderId);
+            Assert.Equal(orderId, charge.OrderId);
+        }
+
+        [Fact]
+        public async Task Full_Charge_After_Authorization_Heidelpay()
+        {
+            var orderId = GetRandomId();
+
+            var card = await Heidelpay.CreatePaymentTypeAsync(PaymentTypeCardNo3DS);
+            var auth = await Heidelpay.AuthorizeAsync(new Authorization(card) { Amount = 1m, Currency = "EUR", ReturnUrl = TestReturnUri, OrderId = orderId });
+            var charge = await Heidelpay.ChargeAuthorizationAsync(auth.PaymentId);
+
+            Assert.NotNull(charge?.Id);
+            Assert.Equal(orderId, auth.OrderId);
+            Assert.Equal(orderId, charge.OrderId);
+        }
+
+        [Fact]
+        public async Task Partial_Charge_After_Authorization()
+        {
+            var card = await Heidelpay.CreatePaymentTypeAsync(PaymentTypeCardNo3DS);
+            var auth = await Heidelpay.AuthorizeAsync(new Authorization(card) { Amount = 1m, Currency = "EUR", ReturnUrl = TestReturnUri });
+
+            var charge = await auth.ChargeAsync(0.1m);
+
+            Assert.NotNull(charge?.Id);
+            Assert.Equal(0.1m, charge.Amount);
         }
     }
 }
