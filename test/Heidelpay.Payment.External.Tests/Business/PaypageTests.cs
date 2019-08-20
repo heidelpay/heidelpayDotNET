1fe0796c728c01ef02f4eb66a4c55bb9da96be42
Berghold-Wieser Thomas <thomas.berghold-wieser@tieto.com>
2019-08-20 16:05:32 +0200

    adding tests

--- test/Heidelpay.Payment.External.Tests/Business/PaypageTests.cs
+++ test/Heidelpay.Payment.External.Tests/Business/PaypageTests.cs
@@ -23,5 +23,35 @@ namespace Heidelpay.Payment.External.Tests.Business
 
             Assert.NotNull(paypage?.RedirectUrl);
         }
+
+        [Fact]
+        public async Task Paypage_Maximum()
+        {
+            var customer = await Heidelpay.CreateCustomerAsync(GetMaximumCustomerSameAddress(GetRandomId()));
+            var basket = await Heidelpay.CreateBasketAsync(GetMaximumBasket());
+            var metadata = await Heidelpay.CreateMetadataAsync(TestMetaData);
+
+            var built = new Paypage(Heidelpay)
+            {
+                Amount = 123,
+                Currency = "EUR",
+                ReturnUrl = TestReturnUri,
+                CustomerId = customer.Id,
+                BasketId = basket.Id,
+                MetadataId = metadata.Id,
+
+                ShopDescription = "My personal shop",
+                ShopName = "Shop.de",
+                ContactUri = new Uri("mailto:rene.felder@heidelpay.com"),
+                HelpUri = new Uri("https://www.heidelpay.com/de/support/"),
+                ImpressumUri = new Uri("https://www.heidelpay.com/de/impressum/"),
+                PrivacyPolicyUri = new Uri("https://www.heidelpay.com/de/datenschutz/"),
+                TermsAndConditionUri = new Uri("https://www.heidelpay.com/de/datenschutz/"),
+            };
+
+            var paypage = await Heidelpay.PaypageAsync(built);
+
+            Assert.NotNull(paypage?.RedirectUrl);
+        }
     }
 }
